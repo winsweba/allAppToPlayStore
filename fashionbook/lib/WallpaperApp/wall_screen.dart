f5a8aa8f2d5adce4d19e039c06a03cadd452a3c5
winsweba <winsweb@outlook.com>
2021-04-12 01:44:08 -0700

    add

--- fashionbook/lib/WallpaperApp/wall_screen.dart
+++ fashionbook/lib/WallpaperApp/wall_screen.dart
@@ -89,3 +89,14 @@ class _WallScreenState extends State<WallScreen> {
       ));
   }           
 }
+
+
+// Fluttertoast.showToast(
+//         msg: "This is Center Short Toast",
+//         toastLength: Toast.LENGTH_SHORT,
+//         gravity: ToastGravity.CENTER,
+//         timeInSecForIosWeb: 1,
+//         backgroundColor: Colors.red,
+//         textColor: Colors.white,
+//         fontSize: 16.0
+//     );
