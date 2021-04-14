4baab300968e0c5e84e25a8f322320f553641aee
winsweba <winsweb@outlook.com>
2021-04-14 00:54:19 -0700

    add

---
+++ roomdesigner/lib/WallpaperApp/wall_screen.dart
@@ -0,0 +1,102 @@
+
+
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:roomdesigner/WallpaperApp/fullscreen_image.dart';
+
+const String testDevice = '';
+
+class WallScreen extends StatefulWidget {
+
+  @override
+  _WallScreenState createState() => new _WallScreenState();
+}
+
+class _WallScreenState extends State<WallScreen> {
+
+  StreamSubscription<QuerySnapshot> subscription;
+  List<DocumentSnapshot> wallpapersList;
+  final CollectionReference collectionReference =
+      FirebaseFirestore.instance.collection("roomdesign");
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    subscription = collectionReference.snapshots().listen((datasnapshot) {
+      setState(() {
+        wallpapersList = datasnapshot.docs;
+      });
+    });
+
+    // _currentScreen();
+  }
+
+  @override
+  void dispose() {
+    subscription?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Fashion Book"),
+        ),
+        body: wallpapersList != null
+            ? new StaggeredGridView.countBuilder(
+                padding: const EdgeInsets.all(8.0),
+                crossAxisCount: 4,
+                itemCount: wallpapersList.length,
+                itemBuilder: (context, i) {
+
+                  String imgPath = wallpapersList[i].get("art");
+
+                  return new Material(
+                    elevation: 8.0,
+                    borderRadius:
+                        new BorderRadius.all(new Radius.circular(8.0)),
+                    child: new InkWell(
+                      onTap: () => Navigator.push(
+                            context,
+                            new MaterialPageRoute(
+                              builder: (context) => new FullScreenImagePage(imgPath),
+                            ),
+                      ),
+                      child: new Hero(
+                        tag: imgPath,
+                        child: new FadeInImage(
+                          image: new NetworkImage(imgPath),
+                          fit: BoxFit.cover,
+                          placeholder: new AssetImage("assets/wallfy.png"),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+                staggeredTileBuilder: (i) =>
+                    new StaggeredTile.count(2, i.isEven ? 2 : 3),
+                mainAxisSpacing: 8.0,
+                crossAxisSpacing: 8.0,
+              )
+            : new Center(
+                child: new CircularProgressIndicator(),
+      ));
+  }           
+}
+
+
+// Fluttertoast.showToast(
+//         msg: "This is Center Short Toast",
+//         toastLength: Toast.LENGTH_SHORT,
+//         gravity: ToastGravity.CENTER,
+//         timeInSecForIosWeb: 1,
+//         backgroundColor: Colors.red,
+//         textColor: Colors.white,
+//         fontSize: 16.0
+//     );
